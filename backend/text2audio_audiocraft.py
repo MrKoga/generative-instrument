4594a4ea4b3487e98afad32e6b8829165912b536
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-03-31 22:14:24 +0900

    add comment

--- backend/text2audio_audiocraft.py
+++ backend/text2audio_audiocraft.py
@@ -1,10 +1,28 @@
+"""
+Official documentation: https://github.com/facebookresearch/audiocraft/tree/main
+
+Installations
+# Best to make sure you have torch installed first, in particular before installing xformers.
+# Don't run this if you already have PyTorch installed.
+python -m pip install 'torch==2.1.0'
+# You might need the following before trying to install the packages
+python -m pip install setuptools wheel
+# Then proceed to one of the following
+python -m pip install -U audiocraft  # stable release
+python -m pip install -U git+https://git@github.com/facebookresearch/audiocraft#egg=audiocraft  # bleeding edge
+python -m pip install -e .  # or if you cloned the repo locally (mandatory if you want to train).
+"""
+
+# How to use
+# python text2audio_audiocraft.py
+
 import torchaudio
 from audiocraft.models import AudioGen
 from audiocraft.data.audio import audio_write
 
 model = AudioGen.get_pretrained('facebook/audiogen-medium')
 model.set_generation_params(duration=5)  # generate 5 seconds.
-descriptions = ['dog barking', 'sirenes of an emergency vehicule', 'footsteps in a corridor']
+descriptions = ['dog barking', 'sirenes of an emergency vehicule', 'footsteps in a corridor'] # prompt
 wav = model.generate(descriptions)  # generates 3 samples.
 
 for idx, one_wav in enumerate(wav):
