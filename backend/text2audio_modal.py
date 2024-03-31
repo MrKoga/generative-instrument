1acd91fb190425cc2c57b9a6cf30ec962bc1afb3
yuzu2yan <al21017@shibaura-it.ac.jp>
2024-03-31 22:41:57 +0900

    add comment

--- backend/text2audio_modal.py
+++ backend/text2audio_modal.py
@@ -1,12 +1,27 @@
+"""
+Official documentation: https://github.com/facebookresearch/audiocraft/tree/main
+
+Installations
+# Best to make sure you have torch installed first, in particular before installing xformers.
+# Don't run this if you already have PyTorch installed.
+python -m pip install 'torch==2.1.0'
+# You might need the following before trying to install the packages
+python -m pip install setuptools wheel
+# Then proceed to one of the following
+python -m pip install -U audiocraft  # stable release
+python -m pip install -U git+https://git@github.com/facebookresearch/audiocraft#egg=audiocraft  # bleeding edge
+python -m pip install -e .  # or if you cloned the repo locally (mandatory if you want to train).
+"""
+
+
 from pathlib import Path
-import io
 
 from modal import Image, Stub, method, gpu
 
-MAX_SEGMENT_DURATION = 30
-
 stub = Stub("audiocraft")
 
+# How to use 
+# modal run text2audio_modal.py --prompt "<prompt>"
 
 def download_models():
     from audiocraft.models import AudioGen
@@ -70,5 +85,4 @@ def main(prompt: str, duration: int = 10, format: str = "wav"):
 
     print("Saving the file")
     for idx, one_wav in enumerate(wav):
-        # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
         audio_write(f'{idx}', one_wav.cpu(), sample_rate, strategy="loudness", loudness_compressor=True)
